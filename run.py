9c1c785039d3698cfd72fbec7e1048c72a7ddfc9
Shukurjon Nematov <shjonbek1702@gmail.com>
2026-09-11 14:16:40 +0900

    32. python variables

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
-print('Hello, World!')
+#Dunder __builtins__ , __init__,
+
+message =  'everything is working fine'
+print(message)
+
+result = type(message)
+print(result)
+
+'''
+In Python, there are built-in tools:
+1) TYPES > int flooat str list dict set tuple bool complex
+2) FUNCTIONS > print() input() len() type() range() sum() min() max() sorted()
+3) CONSTANTS > True False None
+'''
+
+print(dir(__builtins__))  
